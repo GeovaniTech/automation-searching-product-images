e7577ff88f7fb325781fb06fde5b14cf1c9d5ddb
unknown <geovanidebastiani32@gmail.com>
2021-11-28 17:28:13 -0300

    inserting as images into the spreadsheet

--- Automação.py
+++ Automação.py
@@ -1,32 +1,54 @@
-import time
 import pandas as pd
-import webbrowser
 import pyautogui
+
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+from openpyxl import *
+from openpyxl.drawing.image import Image
+
+from skimage.transform import resize
+import matplotlib.pyplot as plt
 
 sheet = pd.read_excel('Produtos.xlsx')
 datas = sheet[['Marca', 'Produto', 'Cor']]
 
 pyautogui.PAUSE = 1
 
-
+row = 1
+ant_row = 0
 for value in datas.values:
     search = value[0] + ' ' + value[1] + ' ' + value[2]
+
     driver = webdriver.Chrome(r'C:\Users\Geovani Debastiani\.wdm\drivers\chromedriver\win32\95.0.4638.69\chromedriver.exe')
     driver.get('https://www.google.com.br/imghp?hl=pt-BR&tab=ri&ogbl')
-    time.sleep(1)
     box = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
     box.send_keys(search)
     box.send_keys(Keys.ENTER)
-    time.sleep(1)
 
     try:
-        box = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img').screenshot(f'Images/{search}.png')
+        driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img').click()
+        driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div[1]/a[1]/div[1]/img').screenshot(f'Images/{search}.png')
+
+        ant_row = row
+
+        row += 1
     except:
+        if row == ant_row:
+            row += 1
         pass
 
+    wb = load_workbook('Produtos.xlsx')
 
+    sheet = wb['Produtos']
+
+    loc_img = r'C:\Users\Geovani Debastiani\Desktop\Automação - Busca por imagens\Images\{}.png'.format(search)
+
+    im = plt.imread(loc_img)
+    res = resize(im, (187, 156))
+
+    img = Image(loc_img)
+    sheet.add_image(img, f'D{row}')
+    wb.save('Produtos.xlsx')
 
