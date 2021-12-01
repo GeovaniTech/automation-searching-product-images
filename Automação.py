bdca11787f89c8ee80b8225e614f5dc65e513410
unknown <geovanidebastiani32@gmail.com>
2021-12-01 20:53:34 -0300

    Resize Images Automatic

--- Automação.py
+++ Automação.py
@@ -1,15 +1,18 @@
+import time
+
+import numpy as np
 import pandas as pd
 import pyautogui
-
+import matplotlib.pyplot as plt
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from openpyxl import *
-from openpyxl.drawing.image import Image
-
+from PIL import Image
+from openpyxl.drawing.image import Image as ImgOpen
 from skimage.transform import resize
-import matplotlib.pyplot as plt
+
 
 sheet = pd.read_excel('Produtos.xlsx')
 datas = sheet[['Marca', 'Produto', 'Cor']]
@@ -40,15 +43,21 @@ for value in datas.values:
         pass
 
     wb = load_workbook('Produtos.xlsx')
-
     sheet = wb['Produtos']
 
-    loc_img = r'C:\Users\Geovani Debastiani\Desktop\Automação - Busca por imagens\Images\{}.png'.format(search)
+    loc_img = r'C:\Users\Geovani Debastiani\Desktop\Development\Projects\Automação - Busca por imagens\Images\{}.png'.format(search)
 
-    im = plt.imread(loc_img)
-    res = resize(im, (187, 156))
+    #im = plt.imread(loc_img)
+    #res = resize(im, (300, 300))
 
-    img = Image(loc_img)
+
+    img = Image.open(loc_img)
+
+    new_img = img.resize((187, 156))
+    new_img.save(loc_img)
+
+    img = ImgOpen(loc_img)
+
     sheet.add_image(img, f'D{row}')
     wb.save('Produtos.xlsx')
 
